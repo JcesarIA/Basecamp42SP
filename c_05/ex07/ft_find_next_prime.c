4c90dbb57636a157803ad29ef95f70394c1c44b8
jcesar-d <jcesar-d@student.42sp.org.br>
2021-07-31 02:38:54 +0000

    c_05

---
+++ c_05/ex07/ft_find_next_prime.c
@@ -0,0 +1,24 @@
+int	ft_is_prime(int nb)
+{
+	int	n;
+
+	n = 2;
+	if (nb < 2)
+		return (0);
+	while (n <= nb / 2)
+	{
+		if (nb % n == 0)
+			return (0);
+		n++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	while (ft_is_prime(nb) != 1 && nb < 2147483647)
+	{
+		nb ++;
+	}
+	return (nb);
+}
